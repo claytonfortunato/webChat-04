c0ac8c8bebb2d481dc0433fac4cdef8ae75881ef
claytonfortunato <claytonreis10@gmail.com>
2023-03-28 22:57:22 -0300

    feat: projeto finalizado

--- src/components/ChatMessage/styles.ts
+++ src/components/ChatMessage/styles.ts
@@ -1,9 +1,21 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+type BackProps = {
+  you: boolean;
+};
 
 export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
   margin: 1rem;
 `;
 
-export const Background = styled.div<{ you: boolean }>`
-  background-color: ${(props) => (props.you ? "#25cd89" : "#16195c")};
+export const Background = styled.div<BackProps>`
+  background-color: ${(props) =>
+    props ? "rgb(7, 132, 126)" : " rgb(99, 59, 188)"};
+  margin-top: 10px;
+  padding: 10px;
+  border-radius: 0 0.8rem 0.8rem 0.8rem;
 `;
